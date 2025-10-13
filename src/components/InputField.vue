4afaf6bb86ea65d114ccb8a051fd6210a067bcc0
Pedro <pedroe8@hotmail.com>
2025-10-13 19:13:42 -0300

    initialize Vue.js project with essential files and configurations

---
+++ src/components/InputField.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <div class="field">
+        <label :for="this.id" class="label">{{ this.label }}</label>
+        <div class="control">
+            <input :type="this.type" :placeholder="this.placeholder" :id="this.id" />
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: "InputField",
+    props:{
+        placeholder:{
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+        id: {
+            type: String,
+            required: true,
+        },
+    },
+};
+
+</script>
---
+++ src/views/UserLogin.vue
@@ -0,0 +1,20 @@
+<template>
+
+    
+        <CardBox>
+            <InputField placeholder="exemple@mail.com" type="email" label="E-mail" id="email"/>
+        </CardBox>
+
+</template>
+
+<script>
+import { CardBox } from '@/components/CardBox.vue';
+import { InputField } from '@/components/InputField.vue';
+
+export default {
+    name: "UserLogin",
+    components: [
+        CardBox, InputField
+    ],
+};
+</script>
